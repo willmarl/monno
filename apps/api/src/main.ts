9dcb8d8695a3a532de3f7bd514603aaad289d7d3
William Marlette <williammarlette3@gmail.com>
2025-12-23 17:17:57 -0500

    fix dockerfile to install and build properly

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 Logger.log('Server running on port ' + process.env.PORT);
+Logger.log('Database URL ' + process.env.DATABASE_URL);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3001);
