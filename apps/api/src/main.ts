610fad291d33069b90951b3d2024c22ff0548f3d
William Marlette <williammarlette3@gmail.com>
2025-12-23 17:30:22 -0500

    add CORS and print env on server start

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger as Print } from '@nestjs/common';
 
-Logger.log('Server running on port ' + process.env.PORT);
+Print.log('Server running on port ' + process.env.PORT);
+Print.log('Database URL ' + process.env.DATABASE_URL);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: [
+      'http://localhost:3001', // example: your Next.js dev server
+      'http://localhost:3000', // if using same port for FE
+    ],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
