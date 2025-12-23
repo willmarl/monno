b1211294a8a143914861f5f3808d578e130f541b
William Marlette <williammarlette3@gmail.com>
2025-12-23 17:48:03 -0500

    add most of the libs that will be needed by project

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 @Module({
   imports: [
     ConfigModule.forRoot({
