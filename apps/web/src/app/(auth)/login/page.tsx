d000f367d154264410231f435eb46b9cb1e51ade
William Marlette <williammarlette3@gmail.com>
2025-12-31 15:04:24 -0500

    add redirect on auth pages if already loggedin

--- apps/web/src/app/(auth)/login/page.tsx
+++ apps/web/src/app/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
 import LoginForm from "@/features/auth/components/LoginForm";
 import { Card } from "@/components/ui/card";
+import { redirectIfLoggedIn } from "@/features/auth/server";
 
 async function LoginPage() {
+  await redirectIfLoggedIn();
+
   return (
     <div className="flex justify-center mt-20">
       <Card className="p-8 w-full max-w-md">
--- apps/web/src/app/(auth)/register/page.tsx
+++ apps/web/src/app/(auth)/register/page.tsx
@@ -1,7 +1,10 @@
 import RegisterForm from "@/features/auth/components/RegisterForm";
 import { Card } from "@/components/ui/card";
+import { redirectIfLoggedIn } from "@/features/auth/server";
 
 export default async function RegisterPage() {
+  await redirectIfLoggedIn();
+
   return (
     <div className="flex justify-center mt-20">
       <Card className="p-8 w-full max-w-md">
