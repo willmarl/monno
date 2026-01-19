c57dcc005de2e3551aa0e2d3215546a0910f6fb7
William Marlette <williammarlette3@gmail.com>
2026-01-19 16:41:09 -0500

    add protect to any admin page

--- apps/web/src/app/(admin)/layout.tsx
+++ apps/web/src/app/(admin)/layout.tsx
@@ -1,6 +1,18 @@
+import { requireAuth } from "@/features/auth/server";
+import { redirect } from "next/navigation";
 import Layout from "@/components/layout/admin/Layout";
 
-export default function layout({ children }: { children: React.ReactNode }) {
+export default async function layout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const user = await requireAuth();
+
+  if (user.role != "ADMIN") {
+    redirect("/unauthorized");
+  }
+
   return (
     <div>
       <Layout>{children}</Layout>
