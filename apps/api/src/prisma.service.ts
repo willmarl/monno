b6f27cfcb43a195f6bd9468304c8b3110a395108
William Marlette <williammarlette3@gmail.com>
2026-01-23 09:28:19 -0500

    remove prisma extension

--- apps/api/src/prisma.service.ts
+++ apps/api/src/prisma.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { softDeleteExtension } from './prisma.extension';
 import 'dotenv/config';
 
 @Injectable()
@@ -13,7 +12,5 @@ export class PrismaService extends PrismaClient {
     }
     const adapter = new PrismaPg({ connectionString });
     super({ adapter });
-    // Apply soft delete extension to all post queries
-    this.$extends(softDeleteExtension);
   }
 }
