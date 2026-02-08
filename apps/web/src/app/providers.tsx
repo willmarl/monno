40b618091bcd58a5af8cb79ebc29a8ad4d737335
William Marlette <williammarlette3@gmail.com>
2026-02-08 10:30:42 -0500

    add tooltip to providers

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -5,13 +5,16 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "@/lib/queryClient";
 import { ThemeProvider } from "@/components/theme/theme-provider";
 import { ModalProvider } from "@/components/modal/ModalProvider";
+import { TooltipProvider } from "@radix-ui/react-tooltip";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <ModalProvider>{children}</ModalProvider>
-        <ReactQueryDevtools initialIsOpen={false} />
+        <TooltipProvider>
+          <ModalProvider>{children}</ModalProvider>
+          <ReactQueryDevtools initialIsOpen={false} />
+        </TooltipProvider>
       </ThemeProvider>
     </QueryClientProvider>
   );
