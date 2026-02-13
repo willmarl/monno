74c41ae8c844ba9db70d20843e03ba7abddbfd5d
William Marlette <williammarlette3@gmail.com>
2026-02-12 19:47:00 -0500

    add api and hook for analytics endpoint

--- apps/web/src/app/demo/page.tsx
+++ apps/web/src/app/demo/page.tsx
@@ -2,8 +2,18 @@
 
 import { usePostHogEvents } from "@/hooks/usePostHogEvents";
 import { Button } from "@/components/ui/button";
+import { useBackendAnalytics } from "@/features/analytics/hook";
 
 export default function page() {
+  const { mutate: PierceCaptureEvent } = useBackendAnalytics();
+
+  const handlePierceClick = () => {
+    PierceCaptureEvent({
+      eventName: "pierce_click",
+      data: { foo: "bar" },
+    });
+  };
+
   const { captureEvent } = usePostHogEvents();
 
   const handleClick = () => {
@@ -19,6 +29,7 @@ export default function page() {
     <div>
       <p>Hi. this is a demo page.</p>
       <Button onClick={handleClick}>Click me</Button>
+      <Button onClick={handlePierceClick}>Blockers don't effect me</Button>
     </div>
   );
 }
