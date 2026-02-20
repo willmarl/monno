c5d3ba777f398f29f96bab27b72cdc09785897bd
William Marlette <williammarlette3@gmail.com>
2026-02-20 11:03:31 -0500

    add credits and subscription section to payment tab

---
+++ apps/web/src/components/pages/settings/CreditsSection.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { formatDate } from "@/lib/utils/date";
+import { useRouter } from "next/navigation";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { useUserCreditTransactions } from "@/features/stripe/hooks";
+import { useSessionUser } from "@/features/auth/hooks";
+
+export function CreditsSection() {
+  const router = useRouter();
+  const { data: credits, isLoading, error } = useUserCreditTransactions(1, 5);
+  const { data: user } = useSessionUser();
+
+  if (!user) return null;
+
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Credits : {user.credits}</CardTitle>
+      </CardHeader>
+      <CardContent className="space-y-4">
+        <Button
+          className="cursor-pointer"
+          variant="outline"
+          size="sm"
+          onClick={() => router.push("/purchases/credits")}
+        >
+          Full history
+        </Button>
+        <Table>
+          <TableCaption>Recent transactions.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Date</TableHead>
+              <TableHead>Before</TableHead>
+              <TableHead>After</TableHead>
+              <TableHead>Amount</TableHead>
+              <TableHead>Type</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {credits?.items?.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell className="text-sm">
+                  {formatDate(row.createdAt as any)}
+                </TableCell>
+                <TableCell className="text-sm">{row.balanceBefore}</TableCell>
+                <TableCell className="text-sm">{row.balanceAfter}</TableCell>
+                <TableCell className="text-sm">{row.amount}</TableCell>
+                <TableCell className="text-sm">{row.type}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+}
