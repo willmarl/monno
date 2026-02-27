1498de7c3b307ecfdd0f85aadb86fa4c8c6aeebe
William Marlette <williammarlette3@gmail.com>
2026-02-27 09:11:49 -0500

    fix comment submit button to always be in disabled state

--- apps/web/src/features/comments/schemas/comment.schema.ts
+++ apps/web/src/features/comments/schemas/comment.schema.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const commentSchema = z.object({
-  resourceType: z.enum(["POST", "VIDEO", "ARTICLE", "COMMENT"]),
-  resourceId: z.number().int(),
+  resourceType: z.enum(["POST", "VIDEO", "ARTICLE", "COMMENT"]).optional(),
+  resourceId: z.number().int().optional(),
   content: z.string().min(1).max(2000),
 });
 
