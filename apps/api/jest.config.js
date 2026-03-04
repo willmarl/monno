b123ef892ad52c3ccb78b83443ba925e3a1cdb0d
William Marlette <williammarlette3@gmail.com>
2026-03-03 21:38:55 -0500

    add jest config

---
+++ apps/api/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: 'src',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage',
+  testEnvironment: 'node',
+  roots: ['<rootDir>', '<rootDir>/../'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/$1',
+  },
+};
