5bb11c67851ca3e6460f17c48c2cc2e0efd32d2b
William Marlette <williammarlette3@gmail.com>
2026-03-15 17:09:50 -0400

    add localhost to CORS

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -24,7 +24,11 @@ Print.log('Server running on port ' + process.env.PORT);
 Print.log('Database URL ' + process.env.DATABASE_URL);
 Print.log('Frontend URL ' + process.env.FRONTEND_URL);
 const corsOrigins = process.env.FRONTEND_URL
-  ? [process.env.FRONTEND_URL, `www.${process.env.FRONTEND_URL}`]
+  ? [
+      process.env.FRONTEND_URL,
+      `www.${process.env.FRONTEND_URL}`,
+      'http://localhost:3000',
+    ]
   : ['http://localhost:3000'];
 
 Print.log('CORS origins allowed: ' + JSON.stringify(corsOrigins));
